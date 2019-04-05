3a1d52acc185c509b9b68164aa3295c17b4e1f98
Anscor <xlyanscor@outlook.com>
2019-04-05 17:18:29 +0800

    AVLTree

--- AVLTree/AVLTree.cpp
+++ AVLTree/AVLTree.cpp
@@ -24,49 +24,49 @@ AVLTree::AVLTreeNode::AVLTreeNode(int val) {
 AVLTree::AVLTreeNode::~AVLTreeNode() {}
 
 int AVLTree::AVLTreeNode::Height(AVLTreeNode* root) {
-    // 返回当前结点的高度，若为空则返回-1
+    // 返回当前节点的高度，若为空则返回-1
     return root ? root->height : -1;
 }
 
 void AVLTree::CalcHeight(AVLTreeNode* root) {
-    // 当前结点的高度为左、右子树最高者高度 + 1
+    // 当前节点的高度为左、右子树最高者高度 + 1
     root->height = std::max(AVLTree::AVLTreeNode::Height(root->left),
                             AVLTree::AVLTreeNode::Height(root->right)) +
                    1;
 }
 
-// 左单旋转
-AVLTree::AVLTreeNode* AVLTree::RotateL(AVLTreeNode* root) {
+// 右旋转
+AVLTree::AVLTreeNode* AVLTree::RotateLL(AVLTreeNode* root) {
     AVLTree::AVLTreeNode* temp = root->left;
     root->left = temp->right;
     temp->right = root;
 
-    // 旋转后root与temp结点高度发生了变化，需重新计算
+    // 旋转后root与temp节点高度发生了变化，需重新计算
     CalcHeight(temp);
     CalcHeight(root);
     return temp;
 }
 
-// 右单旋转
-AVLTree::AVLTreeNode* AVLTree::RotateR(AVLTreeNode* root) {
+// 左旋转
+AVLTree::AVLTreeNode* AVLTree::RotateRR(AVLTreeNode* root) {
     AVLTree::AVLTreeNode* temp = root->right;
     root->right = temp->left;
     temp->left = root;
 
-    // 旋转后root与temp结点高度发生了变化，需重新计算
+    // 旋转后root与temp节点高度发生了变化，需重新计算
     CalcHeight(temp);
     CalcHeight(root);
     return temp;
 }
 
 AVLTree::AVLTreeNode* AVLTree::RotateLR(AVLTreeNode* root) {
-    root->left = RotateR(root->left);
-    return RotateL(root);
+    root->left = RotateRR(root->left);
+    return RotateLL(root);
 }
 
 AVLTree::AVLTreeNode* AVLTree::RotateRL(AVLTreeNode* root) {
-    root->right = RotateL(root->right);
-    return RotateR(root);
+    root->right = RotateLL(root->right);
+    return RotateRR(root);
 }
 
 AVLTree::AVLTreeNode* AVLTree::FindMax(AVLTreeNode* root) {
@@ -85,49 +85,49 @@ AVLTree::AVLTreeNode* AVLTree::FindMin(AVLTreeNode* root) {
 
 AVLTree::AVLTreeNode* AVLTree::Insert(AVLTreeNode* root, int val, int& result) {
     result = 0;
-    // 如果当前结点为空，则返回一个新建的结点
+    // 如果当前节点为空，则返回一个新建的节点
     if (!root) {
         root = new AVLTreeNode(val);
         result = 1;
         return root;
     }
 
-    // 当前结点值比要插入的值大，则应向左子树进行插入
+    // 当前节点值比要插入的值大，则应向左子树进行插入
     if (root->val > val) {
-        // 向左子树插入值为val的结点
+        // 向左子树插入值为val的节点
         root->left = Insert(root->left, val, result);
 
-        // 如果在向左子树插入结点后，引起了树的不平衡（即左子树比右子树高2个单位），则需进行旋转调整
+        // 如果在向左子树插入节点后，引起了树的不平衡（即左子树比右子树高2个单位），则需进行旋转调整
         // 因为是向左子树插入，所以左子树的高度必定大于等于右子树的高度
         if (AVLTreeNode::Height(root->left) -
                 AVLTreeNode::Height(root->right) ==
             2) {
-            // 如果是插入的左子树的左子树，则对应LL情况，只需进行左单旋转即可
-            if (val < root->left->val) root = RotateL(root);
+            // 如果是插入的左子节点的左子树，则对应LL情况，只需进行右单旋转即可
+            if (val < root->left->val) root = RotateLL(root);
             // 否则为LR情况，需进行两次旋转
             else
                 root = RotateLR(root);
         }
     }
-    // 当前结点值比要插入的值小，则应向右子树进行插入
+    // 当前节点值比要插入的值小，则应向右子树进行插入
     else if (val > root->val) {
-        // 向左子树插入值为val的结点
+        // 向左子树插入值为val的节点
         root->right = Insert(root->right, val, result);
 
-        // 如果在向右子树插入结点后，引起了树的不平衡（即右子树比左子树高2个单位），则需进行旋转调整
+        // 如果在向右子树插入节点后，引起了树的不平衡（即右子树比左子树高2个单位），则需进行旋转调整
         // 因为是向右子树插入，所以右子树的高度必定大于等于左子树的高度
         if (AVLTreeNode::Height(root->right) -
                 AVLTreeNode::Height(root->left) ==
             2) {
-            // 如果是插入的右子树的右子树，则对应RR情况，只需进行右单旋转即可
-            if (val > root->right->val) root = RotateR(root);
+            // 如果是插入的右子树的右子树，则对应RR情况，只需进行左单旋转即可
+            if (val > root->right->val) root = RotateRR(root);
             // 否则为RL情况，需进行两次旋转
             else
                 root = RotateRL(root);
         }
     }
 
-    // 回溯时计算出沿插入路径上所有结点的新高度
+    // 回溯时计算出沿插入路径上所有节点的新高度
     CalcHeight(root);
     return root;
 }
@@ -137,29 +137,29 @@ AVLTree::AVLTreeNode* AVLTree::Remove(AVLTreeNode* root, int val, int& result) {
     if (!root) {
         return nullptr;
     }
-    // 待删除结点位于右子树
+    // 待删除节点位于右子树
     if (root->val < val) {
-        // 在右子树中删除结点
+        // 在右子树中删除节点
         root->right = Remove(root->right, val, result);
         // 如果删除后引起了树的不平衡（即左子树的高度与右子树的高度差为2），则需要进行旋转调整
         if (AVLTreeNode::Height(root->left) -
                 AVLTreeNode::Height(root->right) ==
             2) {
             // 如果右儿子的左子树的高度小于右儿子的右子树的高度，
-            // 则删除的结点位于右儿子的左子树上，不平衡情况对应RR，
-            // 进行右单旋转即可
+            // 则删除的节点位于右儿子的左子树上，不平衡情况对应RR，
+            // 进行左旋转即可
             if (AVLTreeNode::Height(root->right->left) <
                 AVLTreeNode::Height(root->right->right))
-                root = RotateR(root);
-            // 否则删除的结点位于右儿子的右子树上，不平衡情况对应RL，
+                root = RotateRR(root);
+            // 否则删除的节点位于右儿子的右子树上，不平衡情况对应RL，
             // 需进行两次旋转
             else
                 root = RotateRL(root);
         }
     }
-    // 待删除结点位于左子树
+    // 待删除节点位于左子树
     else if (root->val > val) {
-        // 在左子树中删除结点
+        // 在左子树中删除节点
         root->left = Remove(root->left, val, result);
 
         // 如果删除后引起了树的不平衡（即右子树的高度与左子树的高度差为2），则需要进行旋转调整
@@ -167,38 +167,38 @@ AVLTree::AVLTreeNode* AVLTree::Remove(AVLTreeNode* root, int val, int& result) {
                 AVLTreeNode::Height(root->left) ==
             2) {
             // 如果左儿子的右子树的高度小于左儿子的左子树的高度，
-            // 则删除的结点位于左儿子的右子树上，不平衡情况对应LL，
-            // 进行左单旋转即可
+            // 则删除的节点位于左儿子的右子树上，不平衡情况对应LL，
+            // 进行右旋转即可
             if (AVLTreeNode::Height(root->left->right) <
                 AVLTreeNode::Height(root->left->left))
-                root = RotateL(root);
-            // 否则删除的结点位于左儿子的左子树上，不平衡情况对应LR，
+                root = RotateLL(root);
+            // 否则删除的节点位于左儿子的左子树上，不平衡情况对应LR，
             // 需进行两次旋转
             else
                 root = RotateLR(root);
         }
     }
-    // 否则应当删除当前结点
+    // 否则应当删除当前节点
     else {
         result = 1;
         int t;
-        // 当前结点有两个子结点，则从中选取高度大的一个子树进行替代，避免引起树的不平衡
+        // 当前节点有两个子节点，则从中选取高度大的一个子树进行替代，避免引起树的不平衡
         if (root->left && root->right) {
-            // 左子树高度大于右子树时，选取左子树中最大值替代当前结点，并删除原最大值结点
+            // 左子树高度大于右子树时，选取左子树中最大值替代当前节点，并删除原最大值节点
             if (AVLTreeNode::Height(root->left) >
                 AVLTreeNode::Height(root->right)) {
                 AVLTreeNode* temp = FindMax(root->left);
                 root->val = temp->val;
                 root->left = Remove(root->left, temp->val, t);
             }
-            // 否则，选取右子树中最小值替代当前结点，并删除原最小值结点
+            // 否则，选取右子树中最小值替代当前节点，并删除原最小值节点
             else {
                 AVLTreeNode* temp = FindMin(root->right);
                 root->val = temp->val;
                 root->right = Remove(root->right, temp->val, t);
             }
         }
-        // 如果当前结点只有一个子结点或没有子结点，直接删除即可
+        // 如果当前节点只有一个子节点或没有子节点，直接删除即可
         else {
             AVLTreeNode* temp = root;
             root = root->left ? root->left : root->right;
@@ -206,7 +206,7 @@ AVLTree::AVLTreeNode* AVLTree::Remove(AVLTreeNode* root, int val, int& result) {
         }
     }
 
-    // 如果返回结点不为空时，回溯地计算出删除路径上所有结点的新高度
+    // 如果返回节点不为空时，回溯地计算出删除路径上所有节点的新高度
     if (root) CalcHeight(root);
     return root;
 }
@@ -233,12 +233,12 @@ int AVLTree::Remove(int val) {
 int AVLTree::Find(int val) {
     AVLTreeNode* temp = root;
     while (temp) {
-        // 如果当前结点为要查找的值，返回1表示该值存在，查找成功
+        // 如果当前节点为要查找的值，返回1表示该值存在，查找成功
         if (temp->val == val) return 1;
-        // 如果当前结点值小于要查找的值，则表示要查找的值位于右子树上，查找右子树
+        // 如果当前节点值小于要查找的值，则表示要查找的值位于右子树上，查找右子树
         if (temp->val < val)
             temp = temp->right;
-        // 如果当前结点值大于要查找的值，则表示要查找的值位于左子树上，查找左子树
+        // 如果当前节点值大于要查找的值，则表示要查找的值位于左子树上，查找左子树
         else
             temp = temp->left;
     }
--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -5,12 +5,12 @@ class AVLTree {
   private:
     class AVLTreeNode {
       public:
-        int val;              /// 当前结点所存的数值
-        int height;           /// 当前结点的高度
+        int val;              /// 当前节点所存的数值
+        int height;           /// 当前节点的高度
         AVLTreeNode* left;    /// 左子树
         AVLTreeNode* right;   /// 右子树
 
-        // 得到root结点的高度
+        // 得到root节点的高度
         static int Height(AVLTreeNode* root);
 
         AVLTreeNode(int val = 0);
@@ -20,29 +20,29 @@ class AVLTree {
     // 内部树结构
     AVLTreeNode* root;
 
-    // 计算root结点的高度
+    // 计算root节点的高度
     void CalcHeight(AVLTreeNode* root);
 
-    // 取得以root为根的树上的最大值结点
+    // 取得以root为根的树上的最大值节点
     AVLTreeNode* FindMax(AVLTreeNode* root);
 
-    // 取得以root为根的树上的最小值结点
+    // 取得以root为根的树上的最小值节点
     AVLTreeNode* FindMin(AVLTreeNode* root);
 
-    // LL情况，以root为根进行左单旋转
-    AVLTreeNode* RotateL(AVLTreeNode* root);
-    // LR情况，先以root结点左儿子为根进行右单旋转，再以root为根进行左单旋转
+    // LL情况，以root为根进行右旋转
+    AVLTreeNode* RotateLL(AVLTreeNode* root);
+    // LR情况，先以root节点左儿子为根进行左旋转，再以root为根进行右旋转
     AVLTreeNode* RotateLR(AVLTreeNode* root);
-    // RR情况，以root为根进行右单旋转
-    AVLTreeNode* RotateR(AVLTreeNode* root);
-    // RL情况，先以root结点右儿子为根进行左单旋转，再以root为根进行右单旋转
+    // RR情况，以root为根进行左旋转
+    AVLTreeNode* RotateRR(AVLTreeNode* root);
+    // RL情况，先以root节点右儿子为根进行右旋转，再以root为根进行左旋转
     AVLTreeNode* RotateRL(AVLTreeNode* root);
 
-    // 向AVL树内插入值为val的一个结点，返回插入该结点后，整个树的根结点
+    // 向AVL树内插入值为val的一个节点，返回插入该节点后，整个树的根节点
     // result返回插入结果，成功插入则返回1，如果该值已经存在则返回0
     AVLTreeNode* Insert(AVLTreeNode* root, int val, int& result);
 
-    // 在AVL树中删除值为val的结点，如果不存在则跳过，返回删除该结点后，整个树的根结点
+    // 在AVL树中删除值为val的节点，如果不存在则跳过，返回删除该节点后，整个树的根节点
     // result返回删除结果，成功删除则返回1，如果该值不存在则返回0
     AVLTreeNode* Remove(AVLTreeNode* root, int val, int& result);
 
@@ -56,11 +56,11 @@ class AVLTree {
     AVLTree();
     ~AVLTree();
 
-    // 向AVL树内插入值为val的一个结点，若成功插入则返回1，若已存在些结点则返回0
+    // 向AVL树内插入值为val的一个节点，若成功插入则返回1，若已存在些节点则返回0
     int Insert(int val);
-    // 在AVL树中删除值为val的结点，若成功删除则返回1，若该结点不存在则返回0
+    // 在AVL树中删除值为val的节点，若成功删除则返回1，若该节点不存在则返回0
     int Remove(int val);
-    // 在AVL树中查找值为val的结点，该结点存在则返回1，否则返回0
+    // 在AVL树中查找值为val的节点，该节点存在则返回1，否则返回0
     int Find(int val);
 
     // 中序遍历整颗树
